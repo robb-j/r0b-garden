5f892a0bc1291526faf4cfcaab51a60b65c52354
Rob Anderson <robb-j@users.noreply.github.com>
2024-09-30 15:08:46 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,23 @@
 const types = [
   {
     pattern: new URLPattern({ pathname: '/films/:slug/', }),
-    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/films/${result.pathname.groups.slug}.md`
+    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/films/${result.pathname.groups.slug}.md?plain=1`
   },
   {
     pattern: new URLPattern({ pathname: '/notes/tag/:slug/', }),
-    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/labels/${result.pathname.groups.slug}.md`
+    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/labels/${result.pathname.groups.slug}.md?plain=1`
   },
   {
     pattern: new URLPattern({ pathname: '/notes/:slug/', }),
-    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/notes/${result.pathname.groups.slug}.md`
+    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/notes/${result.pathname.groups.slug}.md?plain=1`
   },
   {
     pattern: new URLPattern({ pathname: '/photos/:slug/', }),
-    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/photos/${result.pathname.groups.slug}.md`
+    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/photos/${result.pathname.groups.slug}.md?plain=1`
   },
   {
     pattern: new URLPattern({ pathname: '/tunes/:slug/', }),
-    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/tunes/${result.pathname.groups.slug}.md`
+    href: (result) => `https://github.com/robb-j/r0b-garden/blob/main/content/tunes/${result.pathname.groups.slug}.md?plain=1`
   },
 ]
 
