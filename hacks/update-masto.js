3e4b763a902fab5194e0f8b43666594558944f1e
Rob Anderson <rob@andrsn.uk>
2024-11-02 14:51:07 +0000

    update script

--- hacks/update-masto.js
+++ hacks/update-masto.js
@@ -8,14 +8,17 @@ import { writePage } from '../utils.js'
 // ~ after moving my mastodon server from hyem.tech
 //
 
-const prefix = 'https://hyem.tech/@rob/'
-const toReplace = ['mastodon_status', 'mastodon_card']
+const replacements = [
+  { key: 'mastodon_status', prefix: 'https://hyem.tech/@rob/' },
+  { key: 'mastodon_card', prefix: 'https://hyem.tech/@rob/' },
+  // { key: 'mastodon_media', prefix: 'hyem.tech://media/' },
+]
 
 for (const name of fs.globSync('content/**/*.md')) {
   const { content = '', data = {} } = matter(fs.readFileSync(name, 'utf8'))
   let changed = false
 
-  for (const key of toReplace) {
+  for (const { key, prefix } of replacements) {
     if (!data.refs?.[key]) continue
 
     for (let i = 0; i < data.refs[key].length; i++) {
